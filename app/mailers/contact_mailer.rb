41c0e4c32fb85578da2f46181d266c580e6c368d
Gianpaolo D'Amato <damatohg@gmail.com>
2016-12-05 21:22:15 +0000

    Finals before production

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
-class ContactMailer <ActionMailer::Base
-  default to: 'sidercom@sidercomve.com'
+class ContactMailer < ActionMailer::Base
+  default to: 'damatohg@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
